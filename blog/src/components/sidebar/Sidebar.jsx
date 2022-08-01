921b142bf3e29cf023bb2f54affeee717d75283a
FerBaeza <fbaezahurtado@gmail.com>
2022-08-02 00:43:24 +0200

    profile-sidebar

--- blog/src/components/sidebar/Sidebar.jsx
+++ blog/src/components/sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ export default function Sidebar() {
         <div className="sidebarItem">
         <span className="sidebarTitle">ABOUT ME</span>
         <img
-          src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
+          src="https://images.pexels.com/photos/356147/pexels-photo-356147.jpeg?auto=compress&cs=tinysrgb&w=1600"
           alt=""
         />
         <p>
